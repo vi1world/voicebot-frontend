041094232dedc22598723925bffcf7028a37860b
Hoang Lo <hoangvn1509@gmail.com>
2023-07-16 22:01:01 +0700

    Title change

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -30,7 +30,7 @@ function Title({ setMessages }: Props) {
 
   return (
     <div className="flex justify-between items-center w-full p-4 bg-gray-900 text-white font-bold shadow">
-      <div className="italic">Ava - lead generation assistant</div>
+      <div className="italic">Ava - lead generation assistant demo</div>
       <button
         onClick={resetConversation}
         className={
